35f353223dc6f23645eaa100914c08d5b22cd85e
Ivy Lin <104948596+Ivylin8@users.noreply.github.com>
2023-03-09 22:30:51 +0000

    test

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,23 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "funcs.h"
+#include "caesar.h"
+#include "vigenere.h"
+#include "decrypt.h"
 
-// add your tests here
+
+//TASK B TESTS
+TEST_CASE("Caesar Cipher Case") {
+  CHECK(encryptCaesar("Hello, World!", 10) == "Rovvy, Gybvn!");
+}
+
+//TASK C TESTS
+TEST_CASE("Vigenere Cipher Case") {
+  CHECK(encryptVigenere("Hello, World!", "cake") == "Jevpq, Wyvnd!");
+}
+
+//TASK D TESTS
+TEST_CASE("Decrypt Cipher Case") {
+  CHECK(decryptCaesar("Rovvy, Gybvn!", 10) == "Hello, World!");
+  CHECK(decryptVigenere("Jevpq, Wyvnd!", "cake") == "Hello, World!");
+}
